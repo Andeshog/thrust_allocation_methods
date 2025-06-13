fceb4a1828d3bb962cadb6a64e7a9f9332fca3a8
Anders Høgden <andeshog@stud.ntnu.no>
2025-06-13 11:20:11 +0200

    refactor: python node

---
+++ launch/thrust_allocation_py.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
+
+def generate_launch_description():
+    config = os.path.join(get_package_share_directory('thrust_allocation'), 'config', 'thrust_allocation_config.yaml')
+    allocator_mode = DeclareLaunchArgument(
+        'allocator',
+        default_value='pseudo_inverse',
+        description='Allocator mode to use for thrust allocation',
+        choices=['pseudo_inverse', 'nlp', 'qp', 'maneuvering'],
+    )
+    return LaunchDescription([
+        allocator_mode,
+        Node(
+            package='thrust_allocation',
+            executable='thrust_allocation_node.py',
+            name='thrust_allocation_node_py',
+            output='screen',
+            parameters=[config, {'allocator': LaunchConfiguration('allocator')}]
+        ),
+    ])
--- scripts/thrust_allocation_node.py
+++ scripts/thrust_allocation_node.py
@@ -2,165 +2,87 @@
 
 import rclpy
 from rclpy.node import Node
-from rcl_interfaces.msg import ParameterDescriptor
-from std_msgs.msg import Float64
-from geometry_msgs.msg import Wrench, WrenchStamped
-from thrust_allocation.thrust_allocation import ThrustAllocation
-from custom_msgs.msg import ActuatorSetpoints
-from math import pi
+from thrust_allocation.nlp import NLPAllocator
+from thrust_allocation.qp import QPAllocator
+from thrust_allocation.maneuvering_allocator import ManeuveringAllocator
+from thrust_allocation.pseudo_inverse import PsuedoInverseAllocator
+from geometry_msgs.msg import WrenchStamped
+from std_msgs.msg import Float64MultiArray
 import numpy as np
-from rclpy.qos import QoSProfile, HistoryPolicy, ReliabilityPolicy
-
-best_effort_qos = QoSProfile(
-    history=HistoryPolicy.KEEP_LAST,
-    depth=1,
-    reliability=ReliabilityPolicy.BEST_EFFORT
-)
 
 class ThrustAllocationNode(Node):
     def __init__(self):
-        super().__init__('thrust_allocation_node')
+        super().__init__('thrust_allocation_node_py')
 
-        self.thrust_allocation_ = ThrustAllocation()
-
-        self.init_values()
+        self.tau = np.zeros(3)
+        
+        self.pseudo_inverse_allocator_ = PsuedoInverseAllocator()
+        self.nlp_allocator_ = NLPAllocator()
+        self.qp_allocator_ = QPAllocator()
+        self.maneuvering_allocator_ = ManeuveringAllocator()
 
         self.init_params()
-
-        self.init_subscribers()
-
-        self.init_publishers()
+        self.init_topics()
+        self.init_subscribers_and_publishers()
 
         self.timer_ = self.create_timer(self.dt, self.timer_callback)
 
-    def init_values(self):
-        self.azi_angles_base_ : np.ndarray = np.array([3*pi/4, -3*pi/4, -pi/4, pi/4])
-        self.last_angles_ : np.ndarray = self.azi_angles_base_
-        self.last_thrust_ : np.ndarray = np.array([0, 0, 0, 0])
-        self.tau_ : np.ndarray = np.array([2, 0, 0])
+        self.get_logger().info(f"Thrust Allocation Node initialized with allocator mode: {self.allocator_mode}")
+
+    def init_topics(self):
+        self.declare_parameter('tau_topic', '_')
+        self.declare_parameter('out_topic', '_')
 
     def init_params(self):
-        self.declare_parameter('dt', 0.1)
-        self.dt = self.get_parameter('dt').value
+        self.declare_parameter('py.dt', 0.0)
+        self.dt = self.get_parameter('py.dt').value
 
-        self.declare_parameter('min_rpm', -750.0)
-        self.declare_parameter('max_rpm', 750.0)
-        self.min_rpm = self.get_parameter('min_rpm').value
-        self.max_rpm = self.get_parameter('max_rpm').value
+        self.declare_parameter('allocator', 'pseudo_inverse')
+        self.allocator_mode = self.get_parameter('allocator').value
 
-        self.declare_parameter('configuration.Lx', 1.80)
-        self.declare_parameter('configuration.Ly', 0.80)
-        self.declare_parameter('scaling_factor', 3.5)
-        self.declare_parameter('rpm_thrust_mapping.p_propeller_rpm_to_command', 0.842403)
-        self.declare_parameter('rpm_thrust_mapping.p_thrust_to_propeller_rpm', None, ParameterDescriptor(dynamic_typing=True))
-        self.declare_parameter('single_thruster_min_force', -294.0)
-        self.declare_parameter('single_thruster_max_force', 500.0)
-        self.declare_parameter('topics.control_action_reference', '_')
-        self.declare_parameter('topics.actuator_1_angle', '_')
-        self.declare_parameter('topics.actuator_2_angle', '_')
-        self.declare_parameter('topics.actuator_3_angle', '_')
-        self.declare_parameter('topics.actuator_4_angle', '_')
-        self.declare_parameter('topics.actuator_1_setpoint', '_')
-        self.declare_parameter('topics.actuator_2_setpoint', '_')
-        self.declare_parameter('topics.actuator_3_setpoint', '_')
-        self.declare_parameter('topics.actuator_4_setpoint', '_')
-        
-        self.thrust_allocation_.l_x = self.get_parameter('configuration.Lx').value
-        self.thrust_allocation_.l_y = self.get_parameter('configuration.Ly').value
-        self.thrust_allocation_.motor_rpm_factor = self.get_parameter('scaling_factor').value
-        self.thrust_allocation_.p_propeller_rpm_to_command = self.get_parameter('rpm_thrust_mapping.p_propeller_rpm_to_command').value
-        self.thrust_allocation_.p_thrust_to_propeller_rpm = np.array(self.get_parameter('rpm_thrust_mapping.p_thrust_to_propeller_rpm').value)
-        self.thrust_allocation_.single_thruster_min_force = self.get_parameter('single_thruster_min_force').value
-        self.thrust_allocation_.single_thruster_max_force = self.get_parameter('single_thruster_max_force').value
+    def init_subscribers_and_publishers(self):
+        tau_topic = self.get_parameter('tau_topic').value
+        out_topic = self.get_parameter('out_topic').value
 
-    def init_subscribers(self):
-        azi_topic_1 = self.get_parameter('topics.actuator_1_angle').value
-        azi_topic_2 = self.get_parameter('topics.actuator_2_angle').value
-        azi_topic_3 = self.get_parameter('topics.actuator_3_angle').value
-        azi_topic_4 = self.get_parameter('topics.actuator_4_angle').value
-        self.azi_angle_sub_1_ = self.create_subscription(Float64, azi_topic_1, self.azi_angle_callback_1, 10)
-        self.azi_angle_sub_2_ = self.create_subscription(Float64, azi_topic_2, self.azi_angle_callback_2, 10)
-        self.azi_angle_sub_3_ = self.create_subscription(Float64, azi_topic_3, self.azi_angle_callback_3, 10)
-        self.azi_angle_sub_4_ = self.create_subscription(Float64, azi_topic_4, self.azi_angle_callback_4, 10)
+        self.tau_sub_ = self.create_subscription(WrenchStamped, tau_topic, self.tau_callback, 1)
+        self.out_pub_ = self.create_publisher(Float64MultiArray, out_topic, 1)
 
-        tau_topic = self.get_parameter('topics.control_action_reference').value
-        self.control_input_sub_ = self.create_subscription(Wrench, tau_topic, self.control_input_callback, 10)
-
-    def init_publishers(self):
-        act_topic_1 = self.get_parameter('topics.actuator_1_setpoint').value
-        act_topic_2 = self.get_parameter('topics.actuator_2_setpoint').value
-        act_topic_3 = self.get_parameter('topics.actuator_3_setpoint').value
-        act_topic_4 = self.get_parameter('topics.actuator_4_setpoint').value
-        self.actuator_1_ref_pub_ = self.create_publisher(ActuatorSetpoints, act_topic_1, 10)
-        self.actuator_2_ref_pub_ = self.create_publisher(ActuatorSetpoints, act_topic_2, 10)
-        self.actuator_3_ref_pub_ = self.create_publisher(ActuatorSetpoints, act_topic_3, 10)
-        self.actuator_4_ref_pub_ = self.create_publisher(ActuatorSetpoints, act_topic_4, 10)
-
-        self.tau_actual_publisher_ = self.create_publisher(WrenchStamped, 'tau_actual', 1)
-        self.tau_diff_publisher_ = self.create_publisher(WrenchStamped, 'tau_diff', best_effort_qos)
+    def tau_callback(self, msg: WrenchStamped):
+        self.tau = np.array([msg.wrench.force.x, msg.wrench.force.y, msg.wrench.torque.z])
 
     def timer_callback(self):
-        u_cmd, tau_actual = self.thrust_allocation_.allocate_thrust(self.tau_, self.last_thrust_, self.last_angles_)
-
-        tau_actual_msg = WrenchStamped()
-        tau_actual_msg.header.stamp = self.get_clock().now().to_msg()
-        tau_actual_msg.wrench.force.x = tau_actual[0]
-        tau_actual_msg.wrench.force.y = tau_actual[1]
-        tau_actual_msg.wrench.torque.z = tau_actual[2]
-        self.tau_actual_publisher_.publish(tau_actual_msg)
-
-        tau_diff_msg = WrenchStamped()
-        tau_diff_msg.header.stamp = self.get_clock().now().to_msg()
-        tau_diff_msg.wrench.force.x = self.tau_[0] - tau_actual[0]
-        tau_diff_msg.wrench.force.y = self.tau_[1] - tau_actual[1]
-        tau_diff_msg.wrench.torque.z = self.tau_[2] - tau_actual[2]
-        self.tau_diff_publisher_.publish(tau_diff_msg)
-
-        actuator_msg_1 = ActuatorSetpoints()
-        actuator_msg_2 = ActuatorSetpoints()
-        actuator_msg_3 = ActuatorSetpoints()
-        actuator_msg_4 = ActuatorSetpoints()
-
-        actuator_msg_1.throttle_reference = int(self.thrust_allocation_.thrust_to_command(u_cmd[0]))
-        actuator_msg_1.angle_reference = int(np.rad2deg(u_cmd[4]))
-        actuator_msg_2.throttle_reference = int(self.thrust_allocation_.thrust_to_command(u_cmd[1]))
-        actuator_msg_2.angle_reference = int(np.rad2deg(u_cmd[5]))
-        actuator_msg_3.throttle_reference = int(self.thrust_allocation_.thrust_to_command(u_cmd[2]))
-        actuator_msg_3.angle_reference = int(np.rad2deg(u_cmd[6]))
-        actuator_msg_4.throttle_reference = int(self.thrust_allocation_.thrust_to_command(u_cmd[3]))
-        actuator_msg_4.angle_reference = int(np.rad2deg(u_cmd[7]))
+        if self.allocator_mode == 'nlp':
+            alpha, u, tau_actual = self.nlp_allocator_.allocate(self.tau, self.dt)
         
-        self.last_thrust_ = u_cmd[0:4]
-        self.last_angles_ = u_cmd[4:8]
+        elif self.allocator_mode == 'pseudo_inverse':
+            u, tau_actual = self.pseudo_inverse_allocator_.allocate(self.tau)
+            alpha = np.array([3*np.pi/4, -3*np.pi/4, np.pi/4, -np.pi/4])
 
-        self.actuator_1_ref_pub_.publish(actuator_msg_1)
-        self.actuator_2_ref_pub_.publish(actuator_msg_2)
-        self.actuator_3_ref_pub_.publish(actuator_msg_3)
-        self.actuator_4_ref_pub_.publish(actuator_msg_4)
+        elif self.allocator_mode == 'qp':
+            f_ext, u, alpha, tau_actual = self.qp_allocator_.allocate(self.tau, self.dt)
 
-    def azi_angle_callback_1(self, msg: Float64) -> None:
-        self.last_angles_[0] = msg.data
-
-    def azi_angle_callback_2(self, msg: Float64) -> None:
-        self.last_angles_[1] = msg.data
-
-    def azi_angle_callback_3(self, msg: Float64) -> None:
-        self.last_angles_[2] = msg.data
-
-    def azi_angle_callback_4(self, msg: Float64) -> None:
-        self.last_angles_[3] = msg.data
-
-    def control_input_callback(self, msg: Wrench) -> None:
-        self.tau_[0] = msg.force.x
-        self.tau_[1] = msg.force.y
-        self.tau_[2] = msg.torque.z
+        elif self.allocator_mode == 'maneuvering':
+            xi_p, _, _, _ = self.qp_allocator_.allocate(self.tau, self.dt)
+            self.get_logger().info(f"theta: {self.maneuvering_allocator_.theta}")
+            f_ext, u, alpha, tau_actual = self.maneuvering_allocator_.allocate(self.tau, xi_p, self.dt)
+        
+        else:
+            self.get_logger().error(f"Invalid allocator mode: {self.allocator_mode}")
+            return
+        
+        out_msg = Float64MultiArray()
+        out_msg.data = np.concatenate((u, alpha)).tolist()
+        self.out_pub_.publish(out_msg)
 
     
-
 def main(args=None):
     rclpy.init(args=args)
-    node = ThrustAllocationNode()
-    rclpy.spin(node)
+
+    thrust_allocation_node = ThrustAllocationNode()
+
+    rclpy.spin(thrust_allocation_node)
+
+    thrust_allocation_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
